1d27b592304b2799d12d84366b852adc07a4c9b5
Jason Monroe <jason@trebuchet-debian>
2014-02-09 10:25:53 -0800

    Some simple tests to start off

--- test.for
+++ test.for
@@ -1,3 +1,14 @@
-! this is a comment
-C this is a comment isn't it??
-C Third comment
+C     TEST PROGRAM
+C
+      CALL ADD(2, 3, INTC)
+      WRITE(6, 10) 2, 3, INTC 
+   10 FORMAT ('THE ADDITION OF ', I1, ' AND ', I1, ' IS ', I1)
+      END
+C
+C     ===== SUBROUTINE ADD =====
+C
+      SUBROUTINE ADD(INTA, INTB, INTC)
+C     
+      INTC = INTA + INTB
+      RETURN
+      END 
